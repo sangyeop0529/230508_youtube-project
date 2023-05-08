cfe77d4c523e358f3d4026fc641db1b9e6cce80d
Dell <sangyeop0529@gmail.com>
2023-05-08 15:27:28 +0900

    use query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import SearchHeader from "./components/SearchHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `${keyword}` : "검색결과 없음"}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `${keyword}` : "검색결과 없음"}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
