607845f52bb4efd2bf4cd256fa36a50a88b1df86
Dell <sangyeop0529@gmail.com>
2023-05-08 22:38:09 +0900

    fake youtube

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,7 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
 import VideoCard from "../components/VideoCard";
-import axios from "axios";
+import { search } from "../api/youtube";
+import FakeYoutube from "../api/fakeYoutube";
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -10,10 +11,9 @@ export default function Videos() {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], async () => {
-    return axios
-      .get(`/videos/${keyword ? "search" : "popular"}.json`)
-      .then((res) => res.data.items);
+  } = useQuery(["videos", keyword], () => {
+    const youtube = new FakeYoutube();
+    return youtube.search(keyword);
   });
   return (
     <>
